38d09e42c76a4b362795ba3251f92b24bad1761a
supriyadi <ydiizx@gmail.com>
2021-03-27 03:53:12 +0700

    Exception

--- keyword_grabber.py
+++ keyword_grabber.py
@@ -1,24 +1,29 @@
 import requests
-import concurrent.futures
 import os
+import time
 
 def get_keyword(keyword=None, language="en", limit=500):
     result = list()
     url_base = "https://app.neilpatel.com"
     url_1 = "/api/keyword_info?keyword={}&language=en"
-    url_2 = "https://app.neilpatel.com/api/match_keywords?keyword={}&locId={}&language=en&sortBy=searchVolume&sortMode=desc&limit={}&previousKey={}"
+    url_2 = None
     headers = {'Accept': 'application/json, text/plain, */*',
-                'X-Transaction-ID': '77f3b14367fb49adb3804e1ff25b015e',
-                 'Authorization': 'Bearer app#unlogged__0d703b5bab9916c9afcc25ccaa161724ee956578'}
+               'Authorization': "Bearer app#cognito__50a8296df3fb84acd56da093bf5e1f81900a9cd0",
+               "X-Transaction-ID": "fb4d7bbd7cac4bc6a5fe77b60f6de5ff"
+               }
     req_1 = requests.get(url_base+url_1.format(keyword), headers=headers).json()
     counter = 0
     while len(result) <= limit:
+        if counter == 0:
+            url_2 = "https://app.neilpatel.com/api/match_keywords?keyword={}&locId={}&language=en&sortBy=searchVolume&sortMode=desc&limit={}&previousKey={}"
+        else:
+            url_2 = "https://app.neilpatel.com/api/match_keywords?keyword={}&locId={}&language=en&sortBy=searchVolume&sortMode=desc&limit={}&nextKey={}"
         url_ku = url_2.format(keyword, req_1['locId'], "500", str(counter))
         print(url_ku)
         req_2 = requests.get(url_ku, headers=headers).json()
         for key_sugges in req_2['suggestions']:
             result.append(key_sugges['keyword'])
-        counter += 1
+        counter += 500
     return result
 
 def core(mode, output, limit):
@@ -47,7 +52,7 @@ def core(mode, output, limit):
                 try:
                     f.write(x+"\n")
                 except BaseException as e:
-                    print(e)
+                    print(x)
                     continue
 
 if __name__ == "__main__":
